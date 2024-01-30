a9e00dac975f839d6a4d30cd4bd20e5991e4b98f
Shoaib <shoaibs411@gmail.com>
2024-01-30 20:26:18 +0530

    TF Infra AutoPush - Updated S3 Bucket details & some minor updates

--- data.tf
+++ data.tf
@@ -2,24 +2,24 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket  = "b56-terraform-state--bucket"
-    key     = "${var.ENV}/vpc/terraform.tfstate"
-    region  = "us-east-1"
+    bucket          = "roboshop-terraform-state--bucket"
+    key             = "${var.ENV}/vpc/terraform.tfstate"
+    region          = "us-east-1"
   }
 }
 
 data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "centos7-with-ansible"
-  owners           = ["355449129696"]              
+  most_recent       = true
+  name_regex        = "centos7-with-ansible"
+  owners            = ["355449129696"]              
 }
 
 # Extracting the information of the secret
 data "aws_secretsmanager_secret" "secrets" {
-  name = "roboshop/secrets"
+  name              = "roboshop/secrets"
 }
 
 # Extracting the secret version from the secret
 data "aws_secretsmanager_secret_version" "secret_version" {
-  secret_id = data.aws_secretsmanager_secret.secrets.id
+  secret_id         = data.aws_secretsmanager_secret.secrets.id
 }
